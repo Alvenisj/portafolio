84d35e1d68ab18c91139718d7b55c4ae3f164926
Alvenisj <alvenisj@gmail.com>
2022-04-15 23:09:38 -0500

    Portafolio que aún no tiene el json con la traducción a inglés; falta algunos detalles en el área de proyectos

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleThemeIcon = document.getElementById('toggle-theme__icon');
+const toggleThemeText = document.getElementById('toggle-theme__text');
+
+const toggleColors = document.getElementById('toggle-colors');
+
+const rootStyles =  document.documentElement.style;
+
+
+/*CON ESTE CÓDIGO LE CAMBIAMOS LA CLASE AL "BODY" */
+toggleTheme.addEventListener('click', () => {
+
+    /*CON TOGGLE A MODO DE INTERRUPTOR, SI TIENE LA CLASE LA QUITA, SINO LA PONE */
+    document.body.classList.toggle('dark');
+   
+    if(toggleThemeIcon.src.includes('moon.svg')){
+        toggleThemeIcon.src = "assets/icons/sun.svg";
+        toggleThemeText.textContent = 'Light Mode'
+    } else {
+        toggleThemeIcon.src = "assets/icons/moon.svg";
+        toggleThemeText.textContent = 'Dark Mode';
+
+    }
+});
+
+
+
+toggleColors.addEventListener('click', (e) => {
+
+    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+    
+})
+
+
+
+
