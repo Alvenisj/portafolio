46076d3e3b91c66f95823b8ead2f89def79d00c6
Alvenisj <alvenisj@gmail.com>
2022-04-16 21:39:52 -0500

    Portafolio aún tiene problemas con el json que traduce

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,45 @@ const toggleColors = document.getElementById('toggle-colors');
 
 const rootStyles =  document.documentElement.style;
 
+const flagsElementUno = document.getElementById("flags-es");
+const flagsElementDos = document.getElementById("flags-en");
+
+// CÓDIGO AÚN NO FUNCIONA PORQUE TENGO UN ERROR EN LA FUNCIÓN QUE TRAE EL JSON
+const textsToChange = document.querySelectorAll("[data-section]");
+
+//*********************************************** */
+// BANDERA DE ESPAÑA
+flagsElementUno.addEventListener("click", (e) => {
+
+    changelanguage(e.target.parentElement.dataset.language);
+   
+});
+
+//BANDERA DE INGLATERRA
+flagsElementDos.addEventListener("click", (e) => {
+
+    changelanguage(e.target.parentElement.dataset.language);
+
+})
+
+
+//USAR EL JSON Y CAMBIAR EL TEXTO
+const changelanguage = async language => {
+
+    // BUSCAR CÓMO TRAER UN ARCHIVO JSON Y MOSTRARLO EN EL HTML
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+    
+    //REVISAR PORQUE AÚN TIENE UN ERROR
+    for (const textsToChanges of textsToChange) {
+        const section = textsToChange.dataset.section;
+        const value = textsToChange.dataset.value;
+
+        textsToChange = innerHtml = texts[section][value]
+    }
+};
+
+
 
 /*CON ESTE CÓDIGO LE CAMBIAMOS LA CLASE AL "BODY" */
 toggleTheme.addEventListener('click', () => {
